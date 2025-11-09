3aa2d39f69d05ee6e9699ed76a7c65c9f5a1cc6e
mutiararahmaramadhani-hub <mutiararahmaramadhani@gmail.com>
2025-11-09 21:02:10 +0800

    Add files via upload

---
+++ pert1.py
@@ -0,0 +1,17 @@
+# saya sedang belajar python 
+# code dibaca dari kiri kekanan
+# code dibaca dari atas ke bawah
+
+# cara untuk memindahkan satu baris, arahkan kursor pada baris yg ingin di pindahkan
+# tahan tombol alt, lalu tekan tombol arah bawah atau atas 
+
+# cara untuk mengcopy satu baris full, sama sama dengan diatas 
+# namun ditambahkan tahan tombol shif
+
+#print("Nama saya tiara")
+#print("Saya adalah Mahasiswa")
+#print("Tapi Galak")
+
+x = 9
+y = 6
+print(x * y)
---
+++ pert2.py
@@ -0,0 +1,33 @@
+# belajar variabel
+# cara kita menyimpan nilai 
+
+
+# memberikan nilai (assigment)
+x = 10 
+y = 6
+lebar = 13
+
+print(x + y + lebar)
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lebar =", lebar)
+
+# tings to consider when making variabel 
+# hal2 yang harus diperhatikan ketika membuat variabel
+
+bet99 = 9 # tidak boleh diawali dengan angka 
+jumlah_hutang = 1000000 # menggunakan dua suku kata harus digabungkan dengan _
+jumlahHutang = 1000000 # menggunakan dua suku kata harus digabungkan dengan huruf kapital
+
+# pemanggilan indirect
+z = lebar 
+print ("nilai dari z =", z)
+
+# Tugas
+# 1.nama saya adalah
+nama = "Mutiara Rahma Ramadhani"
+print("nama saya adalah ", nama)
+
+# 2.nim saya adalah
+nim = 25241126
+print("nim saya adalah ", nim)
